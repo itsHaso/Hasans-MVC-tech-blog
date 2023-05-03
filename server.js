3189483c80c6bbb18fb364b193d4289ef0ce1edd
itsHaso <117330115+itsHaso@users.noreply.github.com>
2023-05-03 15:04:25 -0400

    server.js

---
+++ server.js
@@ -0,0 +1,46 @@
+require('dotenv').config();
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers/');
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const helpers = require('./utils/helpers');
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {
+    maxAge: 300000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.set('views', './views')
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(routes);
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT}`));
+});
