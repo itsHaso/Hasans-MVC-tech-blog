ca7f6c043c44f8a8a5018c51a2dfdd9994d71e9d
itsHaso <117330115+itsHaso@users.noreply.github.com>
2023-05-03 18:20:14 -0400

    routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,21 +1,10 @@
-
-// create an index js file to set up routes for our api
-
-// import router class from express module and create instance of Router
 const router = require('express').Router();
 
-// import routes for user routes
 const userRoutes = require('./user-routes');
 
-// import routes for post routes
 const postRoutes = require('./post-routes');
 
-// import routes for comment routes
 const commentRoutes = require('./comment-routes');
-
-// sets up a route for the user endpoints 
-// sets up a route for the post endpoints
-// sets up a route for the comment endpoints
 
 router.use('/user', userRoutes);
 router.use('/post', postRoutes);
